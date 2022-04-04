f2d211df5c07bf928d55977a57bb99399fa47a14
ALEKSANDR GOLIKOV <10749+goalrazor@users.noreply.github.yandex-team.ru>
2022-04-04 15:44:51 +0300

    refactor name of let in polymorph

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,10 +1,10 @@
 export function polymorph() {
-  let len2func = [];
+  let lenTwoFunc = [];
   for (let i = 0; i < arguments.length; i++)
     if (typeof (arguments[i]) == "function")
-      len2func[arguments[i].length] = arguments[i];
+      lenTwoFunc[arguments[i].length] = arguments[i];
   return function () {
-    return len2func[arguments.length].apply(this, arguments);
+    return lenTwoFunc[arguments.length].apply(this, arguments);
   }
 }
 
