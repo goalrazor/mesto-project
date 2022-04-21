1c608b67ab7e167cbe51b9e8d2a5eb3615af3541
vartuom <93790667+vartuom@users.noreply.github.com>
2022-04-21 16:32:20 +0800

    feature popups/preview/forms added

--- src/components/index.js
+++ src/components/index.js
@@ -16,6 +16,7 @@ const profileSection = document.querySelector('.profile');
 export const profileName = profileSection.querySelector('.profile__name');
 export const profileDescription = profileSection.querySelector('.profile__desc');
 export const profileAvatar = profileSection.querySelector('.profile__avatar')
+export const addPlaceButton = document.querySelector('.profile__add-btn');
 export let authorId = '';
 
 const api = new Api(config)
@@ -24,7 +25,7 @@ const api = new Api(config)
 const popupImageElement = new PopupWithImage('.fullscreen-view');
 popupImageElement.setEventListeners();
 
-//создаем элемент попапа с формой редактирования профиля и навешиваем слушатели
+//создаем элемент попапа с формой редактирования ПРОФИЛЯ и передаем колбэк с АПИ
 const popupUserElement = new PopupWithForm('.popup_edit-profile', (userData) => {
     console.log(userData); //TODO for debug
     api.updateUserInfo(userData)
@@ -36,11 +37,44 @@ const popupUserElement = new PopupWithForm('.popup_edit-profile', (userData) => 
         .catch((err) => console.log(`Ошибка ${err.status}`));
 });
 popupUserElement.setEventListeners();
-
-//выбираем кнопку редактирования профиля и навешиваем на нее слушатель открытия попапа с формой
+//выбираем кнопку редактирования ПРОФИЛЯ и навешиваем на нее слушатель открытия попапа с формой
 const profileButton = document.querySelector('.profile__edit-btn');
 profileButton.addEventListener('click', () => {
     popupUserElement.open();
+})
+
+//создаем элемент попапа с формой редактирования АВАТАРА и передаем колбэк с АПИ
+const popupAvatarElement = new PopupWithForm('.popup_avatar-edit', (userData) => {
+    //console.log(userData); //TODO for debug
+    api.updateUserAvatar(userData)
+        .then((res) => {
+            profileAvatar.style.backgroundImage = `url(` + res.avatar + `)`;
+            popupAvatarElement.close();
+        })
+        .catch((err) => console.log(`Ошибка ${err.status}`));
+});
+popupAvatarElement.setEventListeners();
+//выбираем кнопку редактирования АВАТАРА и навешиваем на нее слушатель открытия попапа с формой
+profileAvatar.addEventListener('click', () => {
+    popupAvatarElement.open();
+})
+
+//создаем элемент попапа с формой ДОБАВЛЕНИЯ КАРТОЧКИ и передаем колбэк с АПИ
+const popupNewCardElement = new PopupWithForm('.popup_add-place', (userData) => {
+    //console.log(userData); //TODO for debug
+    api.postNewCard(userData)
+        .then((card) => {
+            addCard(options.cardContainer, new Card(card, '#card', (imgSrc, imgHeading) => {
+                popupImageElement.open(imgSrc, imgHeading);
+            }).createCard());
+            popupNewCardElement.close();
+        })
+        .catch((err) => console.log(`Ошибка ${err.status}`));
+});
+popupNewCardElement.setEventListeners();
+//выбираем кнопку ДОБАВЛЕНИЯ КАРТОЧКИ и навешиваем на нее слушатель открытия попапа с формой добавления карточки
+addPlaceButton.addEventListener('click', () => {
+    popupNewCardElement.open();
 })
 
 const loadContentFromServer = () => {
@@ -59,7 +93,7 @@ const loadContentFromServer = () => {
         }).createCard());
       });
 
-      // console.log(cards); //TODO for debug
+      console.log(cards); //TODO for debug
 
     })
     .catch(err => {
--- src/components/popupWithForm.js
+++ src/components/popupWithForm.js
@@ -31,6 +31,7 @@ export default class PopupWithForm extends Popup {
         this._inputList.forEach(input => {
             this._formValues[input.name] = input.value;
         });
+
         return this._formValues;
     }
 
